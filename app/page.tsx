3fcd424d4854231f1b8e2a4bda4dcbecb5b3b93f
wangzip <17826800084g@gmail.com>
2024-12-25 09:37:03 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from "@/app/ui/acme-logo";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
@@ -7,6 +7,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
+        <img src="/public/hero-desktop.png" alt="" />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -25,8 +26,16 @@ export default function Page() {
             <span>Log in</span>
           </Link>
         </div>
+
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            alt="Hero Image"
+            width={1000}
+            height={1000}
+            className="hidden md:block"
+          />
         </div>
       </div>
     </main>
